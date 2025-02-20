5258f29f79ad0438fa8035f2120e7743a823050f
Austriss <austriszeidmanis@gmail.com>
2025-02-20 22:19:56 +0200

    changed initial alien spawn

--- controllers/ControllerGame.py
+++ controllers/ControllerGame.py
@@ -2,10 +2,14 @@ from models.Game import Game
 from models.Enum.EnumObjectType import EnumObjectType
 from models.GameObject import GameObject
 from models.Enum.EnumObjectDirection import EnumObjectDirection
+from controllers.ControllerAlien import ControllerAlien
+import random
 
 
+UFO_INTERVAL_MIN = 15
+UFO_INTERVAL_MAX = 30
 class ControllerGame():
-    move_down_dist = 1
+    
 
     @staticmethod
     def new_game():
@@ -17,19 +21,24 @@ class ControllerGame():
         player.game_object_type = EnumObjectType.Player
         game.game_objects.append(player)
 
-        row_lenght = 10
-        row_count = 3
+        ROW_LENGHT = 10
+        ROW_COUNT = 3
         map_width, map_height = game.map_size
 
-        offset_x = (map_width - row_lenght) // 2
-        offset_y = (map_height - row_count) // 2
 
-        for x in range(row_lenght):
-            for y in range(row_count):
-                if y == 0:  # first row - red alien
-                    obj_type = EnumObjectType.RedAlien
-                else:
+        offset_x = (map_width - ROW_LENGHT) // 2
+        offset_y = ((map_height - ROW_COUNT) // 2) -3
+
+
+
+        for x in range(ROW_LENGHT):
+            for y in range(ROW_COUNT):
+                if y == 0:
+                    obj_type = EnumObjectType.YellowAlien
+                elif y == 1:
                     obj_type = EnumObjectType.GreenAlien
+                elif y == 2:
+                    obj_type = EnumObjectType.BlueAlien
                 obj_game = GameObject()
                 obj_game.position = (x + offset_x, y + offset_y)
                 obj_game.direction = EnumObjectDirection.Right
@@ -42,10 +51,14 @@ class ControllerGame():
         self.game = game
         self.move_down = False
         self.direction_change = False
+        self.ufo_timer = 0
+        self.ufo_timer_interval = random.randint(UFO_INTERVAL_MIN, UFO_INTERVAL_MAX) * 1000
+        self.has_ufo = False
+        self.ufo = None
 
     def update(self, delta_milisec: float):
         aliens = [obj for obj in self.game.game_objects if obj.game_object_type in
-                  (EnumObjectType.GreenAlien, EnumObjectType.RedAlien)]
+                  (EnumObjectType.GreenAlien, EnumObjectType.YellowAlien, EnumObjectType.BlueAlien)]
         map_width = self.game.map_size[0]
 
         border_check = any(alien.position[0] <= 0 or alien.position[0] >= map_width - 1 for alien in aliens)
@@ -53,7 +66,6 @@ class ControllerGame():
         if border_check and not self.direction_change:
             self.game.move_down = True
             self.direction_change = True 
-
 
             for alien in aliens:
                 if alien.direction == EnumObjectDirection.Right:
@@ -68,3 +80,28 @@ class ControllerGame():
             for alien in aliens:
                 alien.position = (alien.position[0], alien.position[1] + 1)
             self.game.move_down = False
+
+
+        
+        if self.ufo is None:
+            self.ufo_timer += delta_milisec
+            if self.ufo_timer >= self.ufo_timer_interval:
+                self.spawn_ufo()
+        elif self.ufo:
+            if self.ufo.position[0] > self.game.map_size[0] or self.ufo.position[0] < -1:
+                if self.ufo in self.game.game_objects:
+                    self.game.game_objects.remove(self.ufo)
+                self.ufo = None
+                self.ufo_timer_interval = random.randint(UFO_INTERVAL_MIN, UFO_INTERVAL_MAX) * 1000
+                self.ufo_timer = 0
+
+    def spawn_ufo(self):
+        if self.ufo is None:
+            self.ufo = GameObject()
+            self.ufo.position = (0, 1)
+            self.ufo.game_object_type = EnumObjectType.RedAlien
+            self.ufo.direction = EnumObjectDirection.Right
+            self.ufo.movement_speed = 1e-3
+            self.game.game_objects.append(self.ufo)
+            self.ufo_timer = 0
+
