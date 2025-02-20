c9276b67986cd6c4ead54a3285b94993380d5368
Austriss <austriszeidmanis@gmail.com>
2025-02-20 22:17:59 +0200

    random alien shooting

--- controllers/ControllerAlien.py
+++ controllers/ControllerAlien.py
@@ -2,23 +2,63 @@ import pygame
 from models.Enum.EnumObjectDirection import EnumObjectDirection
 from models.GameObject import GameObject
 from models.Game import Game
+from models.Enum.EnumObjectType import EnumObjectType
+import random
+from typing import List
 
 class ControllerAlien:
 
     def __init__(self, alien: GameObject):
         self.alien = alien
         self.step_size = 0.5
-        self.move_timer = 0
+        self.move_time = 0
         self.move_interval = 1000
-    def update(self, game: Game, delta_milisec: float):
-        self.move_timer += delta_milisec
+        self.shoot_timer = 0
+        self.shoot_interval = random.randint(2000, 5000)
+        self.can_shoot = False
 
-        if self.move_timer >= self.move_interval and not game.move_down:
-            self.move_timer = 0
+    def update(self, game: Game, delta_milisec: float):
+        self.move_time += delta_milisec
+
+        if self.move_time >= self.move_interval and not game.move_down:
+            self.move_time = 0
             self.move()
+
+        self.shoot_timer += delta_milisec
+
+
+        #random alien shooting
+        if self.shoot_timer >= self.shoot_interval and not self.can_shoot:
+            self.can_shoot = True
+
+        if self.can_shoot:
+            if random.random() < 0.01:
+                self.shoot(game.game_objects)
+                self.shoot_timer = 0
+                self.shoot_interval = random.randint(2000, 5000)
+                self.can_shoot = False
+            else:
+                self.can_shoot = False
+                self.shoot_timer = 0
+                self.shoot_interval = random.randint(2000, 5000)
 
     def move(self):
         if self.alien.direction == EnumObjectDirection.Right:
             self.alien.position = (self.alien.position[0] + self.step_size, self.alien.position[1])
         elif self.alien.direction == EnumObjectDirection.Left:
             self.alien.position = (self.alien.position[0] - self.step_size, self.alien.position[1])
+
+    def update_ufo(self, ufo: GameObject, game: Game, delta_milisec: float):
+        if ufo.direction == EnumObjectDirection.Right:
+            ufo.position = (ufo.position[0] + ufo.movement_speed * delta_milisec, ufo.position[1])
+        else:
+            ufo.position = (ufo.position[0] - ufo.movement_speed * delta_milisec, ufo.position[1])
+
+    # random alien shooting
+    def shoot(self, game_objects: List[GameObject]):
+        bullet = GameObject()
+        bullet.game_object_type = EnumObjectType.AlienBullet
+        bullet.position = (self.alien.position[0], self.alien.position[1] + 1)
+        bullet.direction = EnumObjectDirection.Down
+        bullet.movement_speed = 1e-3
+        game_objects.append(bullet)
--- controllers/ControllerBullet.py
+++ controllers/ControllerBullet.py
@@ -4,38 +4,60 @@ from typing import List
 from models.Enum.EnumObjectType import EnumObjectType
 from models.Game import Game
 
-class ControllerBullet(GameObject):
+
+UFO_KILL_SCORE = 50
+ALIEN_KILL_SCORE = 10
+
+class ControllerBullet():
 
     @staticmethod
     def update(bullet: GameObject, game_objects: List[GameObject], delta_milisec: float, game: Game):
         
-        if bullet.game_object_type == EnumObjectType.Bullet:
-
-            new_position = list(bullet.position) 
-            new_position[1] -= bullet.movement_speed * delta_milisec
-
-            #in game bounds
-            if new_position[1] < 0:
+        if bullet.game_object_type == EnumObjectType.Bullet or bullet.game_object_type == EnumObjectType.AlienBullet:
+            new_position = list(bullet.position)
+            if bullet.game_object_type == EnumObjectType.Bullet:
+                new_position[1] -= bullet.movement_speed * delta_milisec
+            elif bullet.game_object_type == EnumObjectType.AlienBullet:
+                new_position[1] += bullet.movement_speed * delta_milisec
+            #map bullet bounds
+            if new_position[1] < 0 or new_position[1] > game.map_size[1]:
                 if bullet in game_objects:
                     game_objects.remove(bullet)
                     return
+                
             bullet.position = tuple(new_position)
-            for object in list(game_objects): 
-                if object.game_object_type in (EnumObjectType.RedAlien, EnumObjectType.GreenAlien):
-                    if ControllerBullet.Collision(bullet, object):
+
+            for obj in list(game_objects):
+                if obj.game_object_type in (EnumObjectType.YellowAlien,
+                                                EnumObjectType.GreenAlien,
+                                                EnumObjectType.BlueAlien,
+                                                EnumObjectType.RedAlien) and bullet.game_object_type == EnumObjectType.Bullet:
+
+                    if ControllerBullet.collision(bullet, obj):
                         if bullet in game_objects:
                             game_objects.remove(bullet)
-                        if object in game_objects:
-                            game_objects.remove(object)
-                        game.score += 1
+                        if obj in game_objects:
+                            game_objects.remove(obj)
+                        if obj.game_object_type == EnumObjectType.RedAlien:
+                            game.score += UFO_KILL_SCORE
+                        else:
+                            game.score += ALIEN_KILL_SCORE
+                        return 
+                elif obj.game_object_type == EnumObjectType.Player and bullet.game_object_type == EnumObjectType.AlienBullet:
+                    if ControllerBullet.collision(bullet, obj):
+                        if bullet in game_objects:
+                            game_objects.remove(bullet)
+                            game.player_lives -= 1
                         return
+                        
+
 
     @staticmethod
-    def Collision(bullet: GameObject, alien: GameObject):
-        if (bullet.position[0] < alien.position[0] + 1 and
-            bullet.position[0] + 1 > alien.position[0] and
-            bullet.position[1] < alien.position[1] + 1 and
-            bullet.position[1] + 1 > alien.position[1]):
+    def collision(bullet: GameObject, target: GameObject):
+        if (bullet.position[0] < target.position[0] + 1 and
+            bullet.position[0] + 1 > target.position[0] and
+            bullet.position[1] < target.position[1] + 1 and
+            bullet.position[1] + 1 > target.position[1]):
             return True
         return False
 
