1cf1d5bf51b71d143d4628d148ea75c65493a822
Austriss <austriszeidmanis@gmail.com>
2025-03-08 17:25:22 +0200

    refactor: observer moved to utils

---
+++ utils/Observer/Observer.py
@@ -0,0 +1,17 @@
+class Observer:
+    def on_event(self, event):
+        pass
+
+class Subject:
+    def __init__(self):
+        self._observers = []
+
+    def attach(self, observer):
+        self._observers.append(observer)
+
+    def detach(self, observer):
+        self._observers.remove(observer)
+
+    def notify(self, event=None): 
+        for observer in self._observers:
+            observer.on_event(event)
